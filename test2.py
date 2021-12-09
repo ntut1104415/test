64fc9ee46eb2476ef3d1bf4254fa97e7ead4ba99
ntut1104415 <95857687+ntut1104415@users.noreply.github.com>
2021-12-09 21:15:51 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -8,19 +8,22 @@ temp = st.number_input('溫度的資料收集')
 rpm = st.number_input('轉速資料收集')
 confirm_input=st.button('確認')
 if confirm_input:
-    if rpm >= car_simulation.get('rpm_limit'):
-        st.write('轉速過高')
-    else:
-        st.write('正常') 
     if gas <= car_simulation.get('gas_warning'):
         st.write('油量足夠')
     else:
-        st.write('油箱只剩', gas, '格! 準備加油!!')
+        st.write('準備加油!!')
+    
     if speed>=car_simulation.get('speed_limit'):
        st.write('即將超速')
     else:
        st.write('安全')
+    
     if temp>=car_simulation.get('temp_warning'):
        st.write('過熱')
     else:
        st.write('正常')
+    
+    if rpm >= car_simulation.get('rpm_limit'):
+        st.write('轉速過高')
+    else:
+        st.write('正常') 
