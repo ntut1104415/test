7443f71b308ba11f3faa89bfaa153fcbc0a31dce
ntut1104415 <95857687+ntut1104415@users.noreply.github.com>
2021-12-16 21:22:41 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -4,4 +4,4 @@ st.write('我們的組名: 隨便啦 先孤輪')
 confirm_input=st.button('圖片上傳')
 
 if confirm_input:
-  i=get_image_files('car1.jpg')
+  i=get_image_files('https://github.com/ntut1104415/test123/blob/c54545f1dcb7cb576177315928546f347cb2eb93/car1.jpg')
