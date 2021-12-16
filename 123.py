1b50c03714d401b3e03d61cf28a66367d760006a
ntut1104415 <95857687+ntut1104415@users.noreply.github.com>
2021-12-16 20:34:28 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,5 +1,7 @@
 import streamlit as st
 st.write('我們的組名: 隨便啦 先孤輪')
 
-confirm_input=st.button('確認')
-i=st.flim_input('圖片上傳')
+confirm_input=st.button('圖片上傳')
+
+if confirm_input:
+  i=st.flim_input('圖片上傳')
